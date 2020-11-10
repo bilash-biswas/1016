d75932186e6d18d31bf9a72952a1d3e3403a5131
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-10 16:30:17 +0600

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int a;
+    Scanner input = new Scanner(System.in);
+    a = input.nextInt();
+    System.out.printf("%d minutos\n", 2 * a);
+     }  
+}
